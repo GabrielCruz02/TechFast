64bcada3a4232b19f3a99e86a5a8b014b32ff966
Gabriel Cruz <gabrielsilvacruz2002@gmail.com>
2025-04-07 12:18:49 -0300

    inicio tela login

--- src/main/java/br/com/techfast/Main.java
+++ src/main/java/br/com/techfast/Main.java
@@ -1,4 +1,20 @@
 package br.com.techfast;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/br/com/techfast/view/fxml/login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 520, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) { launch(args); }
 }
